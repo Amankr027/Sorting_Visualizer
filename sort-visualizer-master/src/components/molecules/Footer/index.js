4cfdafb3546a81c830d6ed3e5f0780996e22a988
amankr027 <amanvermay27@gmail.com>
2024-07-25 09:47:20 +0530

    commit

---
+++ sort-visualizer-master/src/components/molecules/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span>
+      </section>
+
+      <section className="Footer__Items">
+        <a
+          href="https://github.com/Amankr027/Sorting_Visualizer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
